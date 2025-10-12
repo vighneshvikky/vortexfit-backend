84552923f1b70b1070bfa671a516a899d51e9aad
vighnesh-vikky <vighnesh55642@gmail.com>
2025-10-13 05:00:37 +0530

    Update CORS to handle multiple origins from env

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,17 @@ import morgan from 'morgan';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 dotenv.config();
+const allowedOrigins = process.env.FRONTEND_URL!.split(',');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
   app.use(cookieParser());
+
+
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: allowedOrigins,
     credentials: true,
   });
 
