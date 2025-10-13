fa0a3861ae63fca51ef4913249d3f0047116a232
vighnesh-vikky <vighnesh55642@gmail.com>
2025-10-13 12:27:56 +0530

    added console to see the monogo port log

--- src/main.ts
+++ src/main.ts
@@ -36,5 +36,7 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 
+  console.log('NestJS is running on http://localhost:3000', process.env.PORT);
+
 }
 bootstrap();
